b7db7f222056e22107efd6ebebf4547b7fef00f4
Akshara-agrawal <aksharaagrawal31@gmail.com>
2026-03-31 00:15:24 +0530

    craeted a board function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './style.css'
 
-function App(){
-  
+export default function Board(){
+  return(
+    <div className='ttt'>
+    <div>
+  <button className='square'>X</button>
+  <button className='square'>X</button>
+  <button className='square'>X</button>
+    </div>
+
+   <div>
+  <button className='square'>X</button>
+  <button className='square'>X</button>
+  <button className='square'>X</button>
+   </div>
+
+   <div>
+  <button className='square'>X</button>
+  <button className='square'>X</button>
+  <button className='square'>X</button>
+   </div>
+    </div>
+  );
 }
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Board />
   </React.StrictMode>
 );
 

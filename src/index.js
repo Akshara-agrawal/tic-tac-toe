173f54f384e3d1c26a1df8ef4d5d024698cfb071
Akshara-agrawal <aksharaagrawal31@gmail.com>
2026-04-01 00:18:50 +0530

    used UseState to save the props

--- src/index.js
+++ src/index.js
@@ -1,30 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
+import { useState } from "react";
 
-function Square({value}) {
-  return <button className="square">X</button>;
+function Square({value, onsqrclick}) {
+  // const[value,setvalue]=useState(null)
+  // function clicked(){
+  //   setvalue('X')
+  // }
+  return <button className="square" onClick={onsqrclick} >{value}</button>;
 }
 
 export default function Board() {
+  const[sqr, setsqr]=useState(Array(9).fill(null))
   return (
     <div className="ttt">
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={sqr[0]} />
+        <Square value={sqr[1]}/>
+        <Square value={sqr[2]}/>
       </div>
 
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={sqr[3]}/>
+        <Square value={sqr[4]}/>
+        <Square value={sqr[5]}/>
       </div>
 
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={sqr[6]}/>
+        <Square value={sqr[7]}/>
+        <Square value={sqr[8]}/>
       </div>
     </div>
   );
